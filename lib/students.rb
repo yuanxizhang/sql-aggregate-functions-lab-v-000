ac751919cc762ba4198c3585a74067252ef596d6
Yancy Zhang <yuanxi.zhang@gmail.com>
2018-11-09 06:18:44 +0000

    Done.

--- lib/students.rb
+++ lib/students.rb
@@ -5,13 +5,17 @@ def highest_student_gpa
 end
 
 def lowest_student_gpa
+  "SELECT MIN(gpa) as lowest_student_gpa FROM students;"
 end
 
 def average_student_gpa
+  "SELECT AVG(gpa) as average_student_gpa FROM students;"
 end
 
 def total_tardies_for_all_students
+  "SELECT SUM(tardies) as total_tardies_for_all_students FROM students;"
 end
 
 def average_gpa_for_9th_grade
+  "SELECT AVG(gpa) as average_9th_grade_student_gpa FROM students WHERE grade=9;"
 end
